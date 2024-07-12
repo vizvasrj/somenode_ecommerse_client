f3a7947137f3f0b6c249c42f200e2dff2af92158
vizvasrj <vizvasrj@gmail.com>
2024-07-12 18:45:41 +0530

    ₹

--- app/components/Common/RangeSlider/index.js
+++ app/components/Common/RangeSlider/index.js
@@ -12,93 +12,93 @@ const Range = createSliderWithTooltip(Slider.Range);
 const { Handle } = Slider;
 
 const handle = props => {
-  const { value, dragging, index, ...restProps } = props;
-  return (
-    <SliderTooltip
-      prefixCls='rc-slider-tooltip'
-      overlay={`$${value}`}
-      visible={dragging}
-      placement='top'
-      key={index}
-    >
-      <Handle value={value} {...restProps} />
-    </SliderTooltip>
-  );
+    const { value, dragging, index, ...restProps } = props;
+    return (
+        <SliderTooltip
+            prefixCls='rc-slider-tooltip'
+            overlay={`₹${value}`}
+            visible={dragging}
+            placement='top'
+            key={index}
+        >
+            <Handle value={value} {...restProps} />
+        </SliderTooltip>
+    );
 };
 
 class RangeSlider extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sliderValue: 50,
-      rangeValue: this.props.defaultValue
+    constructor(props) {
+        super(props);
+        this.state = {
+            sliderValue: 50,
+            rangeValue: this.props.defaultValue
+        };
+    }
+
+    onSliderChange = v => {
+        this.setState({
+            sliderValue: v
+        });
     };
-  }
 
-  onSliderChange = v => {
-    this.setState({
-      sliderValue: v
-    });
-  };
+    onRangeChange = v => {
+        this.setState({
+            rangeValue: v
+        });
+    };
 
-  onRangeChange = v => {
-    this.setState({
-      rangeValue: v
-    });
-  };
+    onAfterSliderChange = value => {
+        this.props.onChange(value);
+    };
 
-  onAfterSliderChange = value => {
-    this.props.onChange(value);
-  };
+    onAfterRangeChange = value => {
+        this.props.onChange(value);
+    };
 
-  onAfterRangeChange = value => {
-    this.props.onChange(value);
-  };
+    render() {
+        const { type, marks, step, defaultValue, max, allowCross } = this.props;
+        const { sliderValue, rangeValue } = this.state;
 
-  render() {
-    const { type, marks, step, defaultValue, max, allowCross } = this.props;
-    const { sliderValue, rangeValue } = this.state;
-
-    return (
-      <>
-        {type === 'slider' ? (
-          <Slider
-            className='slider'
-            dots
-            reverse
-            allowCross={allowCross}
-            step={step}
-            defaultValue={defaultValue}
-            marks={marks}
-            value={sliderValue}
-            onChange={this.onSliderChange}
-            onAfterChange={this.onAfterSliderChange}
-          />
-        ) : (
-          <Range
-            className='slider'
-            pushable={10}
-            allowCross={allowCross}
-            min={1}
-            max={max}
-            step={step}
-            defaultValue={defaultValue}
-            marks={marks}
-            handle={handle}
-            tipFormatter={value => `$${value}`}
-            value={rangeValue}
-            onChange={this.onRangeChange}
-            onAfterChange={this.onAfterRangeChange}
-          />
-        )}
-      </>
-    );
-  }
+        return (
+            <>
+                {type === 'slider' ? (
+                    <Slider
+                        className='slider'
+                        dots
+                        reverse
+                        allowCross={allowCross}
+                        step={step}
+                        defaultValue={defaultValue}
+                        marks={marks}
+                        value={sliderValue}
+                        onChange={this.onSliderChange}
+                        onAfterChange={this.onAfterSliderChange}
+                    />
+                ) : (
+                    <Range
+                        className='slider'
+                        pushable={10}
+                        allowCross={allowCross}
+                        min={1}
+                        max={max}
+                        step={step}
+                        defaultValue={defaultValue}
+                        marks={marks}
+                        handle={handle}
+                        tipFormatter={value => `₹${value}`}
+                        value={rangeValue}
+                        onChange={this.onRangeChange}
+                        onAfterChange={this.onAfterRangeChange}
+                    />
+                )}
+            </>
+        );
+    }
 }
 
 RangeSlider.defaultProps = {
-  type: 'range',
-  allowCross: true
+    type: 'range',
+    allowCross: true
 };
 
 export default RangeSlider;
